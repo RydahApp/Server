9ac749d47243f1d6ab41d0f23f4b3e3f5650d456
Hafiz <hafizalimi77@gmail.com>
2024-04-12 18:10:23 +0100

    setup config and folders

---
+++ main/settings/dev.py
@@ -0,0 +1,14 @@
+import os
+from .base import * # noqa
+from dotenv import load_dotenv
+
+
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
