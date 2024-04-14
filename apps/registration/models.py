fa293657d339280ef4499f4fdabe2bf4f90e9330
habeebllah <lasisihabeeb67@gmail.com>
2024-04-14 12:03:17 +0100

    user model updated

--- apps/registration/models.py
+++ apps/registration/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 from .manager import CustomManager
+from rest_framework_simplejwt.tokens import RefreshToken
+
+AUTH_PROVIDERS = {'apple': 'apple', 'google': 'google', 'email': 'email'}
 
 class User(AbstractBaseUser):
   first_name = models.CharField(max_length=50)
@@ -10,7 +13,7 @@ class User(AbstractBaseUser):
   date_of_birth = models.DateField(null=True)
   last_login = models.DateTimeField(null=True, blank=True)
   date_joined = models.DateTimeField(auto_now_add=True)
-
+  auth_provider = models.CharField(max_length=255, blank=False, null=False, default=AUTH_PROVIDERS.get('email'))
   
   is_active = models.BooleanField(default=True)
   is_admin = models.BooleanField(default=False)
@@ -37,3 +40,10 @@ class User(AbstractBaseUser):
 
   def __str__(self):
       return self.email
+  
+  def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
